066cb40de42484b7ba8e14242d731aa0951374fd
gubin_km <gubin_km@nlmk.com>
2017-11-21 09:47:50 +0300

    test

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+var config = {
+    apiKey: "AIzaSyBv1UBV9PtzavhDOphLg16WXxGzDFJbKcI",
+    authDomain: "interview-5fae6.firebaseapp.com",
+    databaseURL: "https://interview-5fae6.firebaseio.com",
+    projectId: "interview-5fae6",
+    storageBucket: "interview-5fae6.appspot.com",
+    messagingSenderId: "265795474933"
+  };
+var fire = firebase.initializeApp(config);
+export default fire;
